f6f4745a1d3a73a308b8e613b86c055d71f23f6e
Daniil Kargashin <danilo98.24fevral@yandex.ru>
2026-04-26 11:10:39 +0300

    fixed tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,9 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 import pytest
 from unittest.mock import patch, MagicMock
 from src.api import HHApi
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 class TestHHApi:
--- tests/test_db_creator.py
+++ tests/test_db_creator.py
@@ -1,10 +1,9 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 import pytest
 from unittest.mock import patch, MagicMock
 from src.db_creator import DBCreator
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 class TestDBCreator:
